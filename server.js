5fcc6d5b866e17bc786db1bc132f2aba600b84ac
Eric Sowell <ericsowell@erics-mbp-2.apollosrc.root>
2016-04-14 10:32:36 -0500

    fixed port naming maybe

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 
 app.use(express.static('public'));
 
-var port = process.env.port || 3000;
+var port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Example app listening on port ' + port + '!');
 });
