e1d952416c47a20d8ed389a245df1f53589cd680
Eric Sowell <ericsowell@erics-mbp-2.apollosrc.root>
2016-04-14 10:19:12 -0500

    added port stuff

--- server.js
+++ server.js
@@ -4,8 +4,7 @@ var app = express();
 
 app.use(express.static('public'));
 
-
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+var port = process.env.port || 3000;
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port + '!');
 });
